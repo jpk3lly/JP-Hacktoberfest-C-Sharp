02cef5b2a1d5d878dc8387c3dacbdf1f74bb9479
John-Paul Kelly <jpk3lly@hotmail.com>
2022-10-01 23:47:43 +0100

    Add project files.

---
+++ JP Hacktoberfest C Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hacktoberfest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
