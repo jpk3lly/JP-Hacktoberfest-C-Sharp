e09b8ef611f2588c67682f845fb68018aed71d32
John-Paul Kelly <jpk3lly@hotmail.com>
2022-10-05 00:07:42 +0100

    More for Dictionary API

---
+++ JP Hacktoberfest C Sharp/ApiCalls.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP_Hacktoberfest_C_Sharp
+{
+    public class ApiCalls
+    {
+        public async Task<DictionaryModel> LoadDictionary()
+        {
+            string url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+            using (HttpResponseMessage response = await DictionaryAPI.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    DictionaryModel dictionary = await response.Content.ReadAsAsync<DictionaryModel>();
+
+                    return dictionary;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
