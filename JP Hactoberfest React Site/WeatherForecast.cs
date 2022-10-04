6a95bc0107e3f12549e992572d471b1d2f29549d
Thomas <thomas.cowburn@skipton.co.uk>
2022-10-04 14:32:08 +0100

    created basic react site

---
+++ JP Hactoberfest React Site/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace JP_Hactoberfest_React_Site;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
